9dffa4caae6ff3cb8705dd173340f977aa6d318c
Mark Dierolf <mdierolf@cloudquote.io>
2025-07-18 21:03:46 -0700

    Initial commit for @token-ring/database

---
+++ index.js
@@ -0,0 +1,6 @@
+export { default as DatabaseResource } from "./DatabaseResource.js";
+
+export const name = "@token-ring/database";
+export const description = "Abstract database resource definitions.";
+export const version = "0.1.0";
+export * as tools from "./tools.js";
---
+++ tools.js
@@ -0,0 +1,3 @@
+export * as listDatabases from "./tools/listDatabases.js";
+export * as executeSql from "./tools/executeSql.js";
+export * as showSchema from "./tools/showSchema.js";
