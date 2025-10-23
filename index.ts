f874581ed52175dec1742e443c0a0051e50f85c7
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:47 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -10,21 +10,21 @@ export const DatabaseConfigSchema = z.object({
   providers: z.record(z.string(), z.any())
 }).optional();
 
-export const packageInfo: TokenRingPackage = {
+export default {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
   install(agentTeam: AgentTeam) {
     const config = agentTeam.getConfigSlice('database', DatabaseConfigSchema);
     if (config) {
-      agentTeam.addTools(packageInfo, tools);
+      agentTeam.addTools(packageJSON.name, tools);
       agentTeam.addServices(new DatabaseService());
       agentTeam.services.waitForItemByType(IterableService).then((iterableService: IterableService) => {
         iterableService.registerProvider("sql", new SqlIterableProvider());
       });
     }
   }
-};
+} as TokenRingPackage;
 
 export {default as DatabaseProvider} from "./DatabaseProvider.js";
 export {default as DatabaseService} from "./DatabaseService.ts";
