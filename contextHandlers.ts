c871f6ad30a22212f3e0a5b477ec7eb8fc6987f8
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-30 12:07:03 -0800

    Replace import path for `TokenRingToolDefinition` from `types` to `schema`

--- contextHandlers.ts
+++ contextHandlers.ts
@@ -1,4 +1,4 @@
-import {ContextHandler} from "@tokenring-ai/chat/types";
+import {ContextHandler} from "@tokenring-ai/chat/schema";
 import availableDatabases from "./contextHandlers/availableDatabases.ts";
 
 export default {
